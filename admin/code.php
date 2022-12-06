c79e148b6420a776c4e87971473f059067de3451
Trong_Nguyen <61712725+lokinguyen04@users.noreply.github.com>
2022-12-07 05:16:01 +0700

    Trong-update-code.php

--- admin/code.php
+++ admin/code.php
@@ -32,6 +32,62 @@ if (isset($_GET['user_id'])) {
     }
 }
 
+if (isset($_GET['product_id'])) {
+    $product_id = mysqli_real_escape_string($mysqli, $_GET['product_id']);
+
+    $query = "SELECT * FROM product WHERE product_id='$product_id'";
+    $query_run = mysqli_query($mysqli, $query);
+
+    if (mysqli_num_rows($query_run) == 1) {
+        $student = mysqli_fetch_array($query_run);
+
+        $res = [
+            'status' => 200,
+            'message' => 'user Fetch Successfully by id',
+            'data' => $student
+        ];
+        echo json_encode($res);
+        return;
+    } else {
+        $res = [
+            'status' => 404,
+            'message' => 'user Id Not Found'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+
+if (isset($_POST['delete_product'])) {
+    $product_id = mysqli_real_escape_string($mysqli, $_POST['product_id']);
+
+    $query = "DELETE FROM product WHERE product_id='$product_id'";
+   
+
+    $query_run = mysqli_query($mysqli, $query);
+ 
+
+
+    if ($query_run) {
+        $res = [
+            'status' => 200,
+            'message' => 'Product Deleted Successfully'
+        ];
+        echo json_encode($res);
+        return;
+    } else {
+        $res = [
+            'status' => 500,
+            'message' => 'Product Not Deleted'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
+
+
 
 if (isset($_POST['delete_user'])) {
     $user_id = mysqli_real_escape_string($mysqli, $_POST['user_id']);
@@ -60,4 +116,13 @@ if (isset($_POST['delete_user'])) {
         echo json_encode($res);
         return;
     }
+}
+
+
+if (isset($_POST['submit'])) {
+
+    $sql = "INSERT INTO product (product_id,product_name,description,price,quantity,product_image) VALUES ('','$product_name','$desc','$price','$quantity','$image')";
+    $query = mysqli_query($mysqli, $sql);
+    header('location:manageProducts.php');
+    
 }
